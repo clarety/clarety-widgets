d7b110afb77baeacb66e931aca903528011555ab
Adam Pritchard <apritchard@iworks.com.au>
2019-03-29 16:24:48 +1000

    Create SuccessPanel.

--- src/donate/views/DonateWidget.js
+++ src/donate/views/DonateWidget.js
@@ -2,11 +2,13 @@ import React from 'react';
 import AmountPanel from './AmountPanel';
 import DetailsPanel from './DetailsPanel';
 import PaymentPanel from './PaymentPanel';
+import SuccessPanel from './SuccessPanel';
 
 const DonateWidget = () => (
-  <AmountPanel />
+  // <AmountPanel />
   // <DetailsPanel />
   // <PaymentPanel />
+  <SuccessPanel />
 );
 
 export default DonateWidget;
---
+++ src/donate/views/SuccessPanel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SuccessPanel = () => (
+  <h1>Thankyou for your donation!</h1>
+);
+
+export default SuccessPanel;
