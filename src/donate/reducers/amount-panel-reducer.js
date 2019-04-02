1fdb2a700e6ab7625a251894034f7fc04dc5d0ad
Adam Pritchard <apritchard@iworks.com.au>
2019-04-02 12:46:06 +1000

    Refactor amount panel reducer.

--- src/donate/reducers/amount-panel-reducer.js
+++ src/donate/reducers/amount-panel-reducer.js
@@ -7,33 +7,36 @@ const initialState = {
 
 const amountPanelReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.selectFrequency:
-      return {
-        ...state,
-        frequency: action.payload,
-      };
-
-    case actionTypes.selectAmount:
-      const { frequency, index, amount, variableAmount } = action.payload;
-
-      const selections = { ...state.selections };
-
-      selections[frequency] = selections[frequency] || {};
-      selections[frequency].index = index;
-      selections[frequency].amount = amount;
-
-      if (variableAmount !== null) {
-        selections[frequency].variableAmount = variableAmount;
-      }
-
-      return {
-        ...state,
-        selections,
-      };
-
-    default:
-      return state;
+    case actionTypes.selectFrequency: return _selectFrequency(state, action);
+    case actionTypes.selectAmount: return _selectAmount(state, action);
+    default: return state;
   }
+};
+
+const _selectFrequency = (state, action) => {
+  return {
+    ...state,
+    frequency: action.payload,
+  };
+};
+
+const _selectAmount = (state, action) => {
+  const { frequency, index, amount, variableAmount } = action.payload;
+
+  const selections = { ...state.selections };
+
+  selections[frequency] = selections[frequency] || {};
+  selections[frequency].index = index;
+  selections[frequency].amount = amount;
+
+  if (variableAmount !== null) {
+    selections[frequency].variableAmount = variableAmount;
+  }
+
+  return {
+    ...state,
+    selections,
+  };  
 };
 
 export default amountPanelReducer;
