4634599488a1200aade6746541ee2aaf6aa75216
Adam Pritchard <apritchard@iworks.com.au>
2019-04-04 11:09:07 +1000

    Format card number.

--- src/donate/components/payment/CardNumberInput.js
+++ src/donate/components/payment/CardNumberInput.js
@@ -7,11 +7,28 @@ const CardNumberInput = ({ cardNumber, placeholder, testId, updateCardNumber }) 
   <Form.Control
     type="tel"
     placeholder={placeholder || '•••• •••• •••• ••••'}
-    value={cardNumber || ''}
+    value={formatCardNumber(cardNumber || '')}
     onChange={event => updateCardNumber(event.target.value)}
     data-testid={testId}
   />
 );
+
+// Split numbers into groups of 4.
+const formatCardNumber = number => {
+  const groups = [];
+
+  for (let i = 0; i < number.length; i += 4) {
+    const group = number.substring(i, i + 4);
+    groups.push(group);
+  }
+
+  return groups.join(' ');
+}
+
+// Remove anything that isn't a number.
+const cleanCardNumber = number => {
+  return number.replace(/[^0-9]/g, '');
+};
 
 const mapStateToProps = state => {
   return {
@@ -20,7 +37,7 @@ const mapStateToProps = state => {
 };
 
 const actions = {
-  updateCardNumber: cardNumber => updatePaymentPanelData('cardNumber', cardNumber),
+  updateCardNumber: cardNumber => updatePaymentPanelData('cardNumber', cleanCardNumber(cardNumber)),
 };
 
 export default connect(mapStateToProps, actions)(CardNumberInput);
