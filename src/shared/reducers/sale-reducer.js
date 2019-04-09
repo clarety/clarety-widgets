faf63e9ed5f930742da5614dca70c5aac5d905ce
Adam Pritchard <apritchard@iworks.com.au>
2019-04-09 15:58:22 +1000

    Add 'payment' and remove 'total' from sale reducer.

--- src/shared/reducers/sale-reducer.js
+++ src/shared/reducers/sale-reducer.js
@@ -2,26 +2,25 @@ import { actionTypes } from '../actions';
 
 const initialState = {
   saleLines: [],
-  total: 0,
+  payment: {},
 };
 
 export const saleReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.addSaleLine:
-      const saleLines = [
-        ...state.saleLines,
-        action.saleLine,
-      ];
-
-      const total = saleLines.reduce((total, line) => total + line.amount, 0);
-
       return {
-        saleLines,
-        total,
+        ...state,
+        saleLines: [
+          ...state.saleLines,
+          action.saleLine,
+        ],
       };
 
     case actionTypes.clearSaleLines:
-      return initialState;
+      return {
+        ...state,
+        saleLines: [],
+      };
 
     default:
       return state;
