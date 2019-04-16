5939ef6f53366e4f50b22487c3690ac9d98f54fb
Adam Pritchard <apritchard@iworks.com.au>
2019-04-16 16:03:56 +1000

    Remove temp 'InstanceTestActions' class.

--- e2e/integration/donate/donate.spec.js
+++ e2e/integration/donate/donate.spec.js
@@ -1,32 +1,25 @@
 import { DonateTestActions } from './donate-test-actions';
 import { donateWidgetTests, donateTestData } from './donate-widget-tests';
 
-class InstanceTestActions extends DonateTestActions {
-  clickNext() {
-    cy.log('click next from instance!');
-    super.clickNext();
-  }
-}
-
-donateWidgetTests(InstanceTestActions, donateTestData, {
+donateWidgetTests(DonateTestActions, donateTestData, {
   testName: 'One-off donation of suggested amount',
   frequency: 'single',
   isVariable: false,
 });
 
-donateWidgetTests(InstanceTestActions, donateTestData, {
+donateWidgetTests(DonateTestActions, donateTestData, {
   testName: 'One-off donation of variable amount',
   frequency: 'single',
   isVariable: true,
 });
 
-donateWidgetTests(InstanceTestActions, donateTestData, {
+donateWidgetTests(DonateTestActions, donateTestData, {
   testName: 'Monthly donation of suggested amount',
   frequency: 'monthly',
   isVariable: false,
 });
 
-donateWidgetTests(InstanceTestActions, donateTestData, {
+donateWidgetTests(DonateTestActions, donateTestData, {
   testName: 'Monthly donation of variable amount',
   frequency: 'monthly',
   isVariable: true,
