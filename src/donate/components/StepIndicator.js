d1a94f881a4f6990a1377487ccff523c9fd4cdc7
Adam Pritchard <apritchard@iworks.com.au>
2019-05-15 12:12:31 +1000

    Don't include step indicator css.

--- src/donate/components/StepIndicator.js
+++ src/donate/components/StepIndicator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './StepIndicator.css';
+// import './StepIndicator.css';
 
 const StepIndicator = ({ currentStep }) => {
   const styles = _getStyles(currentStep);
