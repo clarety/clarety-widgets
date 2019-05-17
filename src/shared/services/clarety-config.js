007434843bdf71dcc3590a9c3fc46272cc923332
Adam Pritchard <apritchard@iworks.com.au>
2019-05-17 15:16:45 +1000

    Bug fix: Use arrow functions for static methods.

--- src/shared/services/clarety-config.js
+++ src/shared/services/clarety-config.js
@@ -1,15 +1,15 @@
 class ClaretyConfig {
   static _config = null;
 
-  static init(config) {
+  static init = config => {
     this._config = config;
   }
 
-  static get(key) {
+  static get = key => {
     return this._config[key];
   }
 
-  static env() {
+  static env = () => {
     const url = window.location.href;
     if (url.startsWith('http://localhost')) return 'dev';
     if (url.startsWith('http://dev-'))      return 'dev';
@@ -17,9 +17,7 @@ class ClaretyConfig {
     if (url.startsWith('https://stage-'))   return 'stage';
   
     return 'prod';
-  };
-
-  
+  }  
 }
 
 export default ClaretyConfig;
