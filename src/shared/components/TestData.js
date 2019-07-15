95633b8eec3ebee15733fabf7972c439146ca469
Adam Pritchard <apritchard@iworks.com.au>
2019-07-15 11:32:17 +1000

    Tidy up shared components import/export.

--- src/shared/components/TestData.js
+++ src/shared/components/TestData.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ClarityConfig from '../services/clarety-config';
 
-const TestData = ({ testId, data }) => {
+export const TestData = ({ testId, data }) => {
   if (ClarityConfig.get('env') !== 'dev') return null;
 
   return <div data-testid={testId} data-testdata={JSON.stringify(data)}></div>
 };
-
-export default TestData;
