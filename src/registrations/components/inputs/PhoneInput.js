68a9651f140f7a7c23af39324d5b7a48045f1c52
Adam Pritchard <apritchard@iworks.com.au>
2019-07-25 15:05:10 +1000

    Add 'react-phone-number-input' package. Setup phone input component (WIP).

--- src/registrations/components/inputs/PhoneInput.js
+++ src/registrations/components/inputs/PhoneInput.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
+import ReactPhoneNumberInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 import { FormContext } from 'registrations/utils';
 
 class PurePhoneInput extends React.PureComponent {
   render() {
     const { field, value, onChange } = this.props;
+    const country = "IN"; // TODO: get from config...
+    const length = 10; // TODO: get from config...
+    const maxLength = length + 3; // Add 3 to allow for country code.
 
     return (
       <Form.Group controlId={field}>
         <Form.Label>
           <FormattedMessage id={`label.${field}`} />
         </Form.Label>
-        <Form.Control
+        <ReactPhoneNumberInput
           value={value}
-          onChange={event => onChange(field, event.target.value)}
-          type="tel"
+          onChange={value => onChange(field, value)}
+          limitMaxLength={true}
+          // maxlength="12" // TODO: GET FROM CONFIG .....
+          inputClassName="form-control"
+          // countries={['IN']}
+          // international={false}
+          country={country}
         />
       </Form.Group>
     );
