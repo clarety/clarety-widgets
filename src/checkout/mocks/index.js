4faa0d216b2d23b02be3a2ba7a922c305953343d
Adam Pritchard <apritchard@iworks.com.au>
2019-08-15 15:59:52 +1000

    Reorganise mock response directories;

--- src/checkout/mocks/index.js
+++ src/checkout/mocks/index.js
@@ -1,15 +1,14 @@
-export * from './cart/create-customer-success';
-export * from './cart/get-cart-success';
-export * from './cart/get-customer-success';
-export * from './cart/get-shipping-options-success';
-export * from './cart/payment-success';
-export * from './cart/select-shipping-option-success';
-export * from './cart/update-address-success';
-export * from './cart/update-shipping-success';
+export * from './carts/get-cart-success';
+export * from './carts/get-shipping-options-success';
+export * from './carts/payment-success';
+export * from './carts/select-shipping-option-success';
+
+export * from './customers/create-customer-success';
+export * from './customers/customer-search-has-account';
+export * from './customers/customer-search-no-account';
+export * from './customers/get-customer-success';
 
 export * from './login/create-account-success';
-export * from './login/customer-search-has-account';
-export * from './login/customer-search-no-account';
 export * from './login/login-success';
 
 export * from './mock-endpoints';
