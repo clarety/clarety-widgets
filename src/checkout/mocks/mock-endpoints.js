30dfe31a5f0ec66617df2c087f790ffb3ac02e74
Adam Pritchard <apritchard@iworks.com.au>
2019-08-15 15:52:35 +1000

    Tidy up mock responses imports;

--- src/checkout/mocks/mock-endpoints.js
+++ src/checkout/mocks/mock-endpoints.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { ClaretyApi } from 'clarety-utils';
-import { customerSearchHasAccount, customerSearchNoAccount, createAccountSuccess, getCustomerSuccess } from 'checkout/mocks';
-import { loginSuccess, getCartSuccess, updateAddressSuccess, updateShippingSuccess, paymentSuccess } from 'checkout/mocks';
-import { createCustomerSuccess, getShippingOptionsSuccess, selectShippingOptionSuccess } from 'checkout/mocks';
+import * as responses from 'checkout/mocks';
 
 export function setupCheckoutAxiosMock() {
   const mock = new MockAdapter(axios, { delayResponse: 2000 });
@@ -15,69 +11,66 @@ export function setupCheckoutAxiosMock() {
   // Login.
   mock
     .onPost(`${apiBase}jwttoken/`)
-    .reply(200, loginSuccess);
+    .reply(200, responses.loginSuccess);
 
   // Get cart.
   mock
     .onGet(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/`)
-    .reply(200, getCartSuccess);
+    .reply(200, responses.getCartSuccess);
 
   // Get customer.
   mock
     .onGet(`${apiBase}carts/customers/e7fb8831-4a83-468e-8eec-593185909f18/`)
-    .reply(200, getCustomerSuccess);
+    .reply(200, responses.getCustomerSuccess);
 
   // Create customer.
   mock
     .onPost(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/customers/`)
-    .reply(200, createCustomerSuccess);
+    .reply(200, responses.createCustomerSuccess);
 
   // Update customer.
   mock
     .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/customers/e7fb8831-4a83-468e-8eec-593185909f18/`)
-    .reply(200, createCustomerSuccess);
+    .reply(200, responses.createCustomerSuccess);
 
   // Get shipping options.
   mock
     .onGet(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/`)
-    .reply(200, getShippingOptionsSuccess);
+    .reply(200, responses.getShippingOptionsSuccess);
 
   // Select shipping option.
   mock
     .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/f5e3ad54-e097-4b6d-8db0-3986916cfb86/`)
-    .reply(200, selectShippingOptionSuccess);
+    .reply(200, responses.selectShippingOptionSuccess);
 
   mock
     .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/f5e68a7d-22ad-4733-b730-a9ef8c4c6bb0/`)
-    .reply(200, selectShippingOptionSuccess);
+    .reply(200, responses.selectShippingOptionSuccess);
 
 
+  // TODO: remove these soon...
 
   mock
     .onGet(`${apiBase}customer-search/`)
     .reply(request => {
       const response = request.params.email === 'test@test.com'
-                     ? customerSearchHasAccount
-                     : customerSearchNoAccount;
+                     ? responses.customerSearchHasAccount
+                     : responses.customerSearchNoAccount;
 
       return [200, response];
     });
 
   mock
     .onPost(`${apiBase}customer-new/`)
-    .reply(200, createAccountSuccess);
+    .reply(200, responses.createAccountSuccess);
 
   mock
     .onPost(`${apiBase}checkout/`)
     .reply(request => {        
       const data = JSON.parse(request.data);
 
-      if (data.payment) return [200, paymentSuccess];
+      if (data.payment) return [200, responses.paymentSuccess];
 
-      if (data.shippingOption) return [200, updateShippingSuccess];
-
-      if (data.customer.delivery) return [200, updateAddressSuccess];
-
-      return [200, getCartSuccess];
+      return [200, responses.getCartSuccess];
     });
 }
