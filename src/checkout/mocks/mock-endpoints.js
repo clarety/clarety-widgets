785cad3a68abdfc7535dc8e768859ff3b6356956
Adam Pritchard <apritchard@iworks.com.au>
2019-08-16 10:29:09 +1000

    Tidy up mock endpoints;

--- src/checkout/mocks/mock-endpoints.js
+++ src/checkout/mocks/mock-endpoints.js
@@ -6,68 +6,27 @@ import * as responses from 'checkout/mocks';
 export function setupCheckoutAxiosMock() {
   const mock = new MockAdapter(axios, { delayResponse: 2000 });
 
-  const apiBase = ClaretyApi.getApiBaseUrl();
+  const api = ClaretyApi.getApiBaseUrl().slice(0, -1);
+
+  // Mock uids.
+  const cartUid            = '8c2756b2-f018-4c27-a025-c31fca7e482b';
+  const customerUid        = 'e7fb8831-4a83-468e-8eec-593185909f18';
+  const shippingOption1Uid = 'f5e3ad54-e097-4b6d-8db0-3986916cfb86';
+  const shippingOption2Uid = 'f5e68a7d-22ad-4733-b730-a9ef8c4c6bb0';
 
   // Login.
   mock
-    .onPost(`${apiBase}jwttoken/`)
+    .onPost(`${api}/jwttoken/`)
     .reply(200, responses.loginSuccess);
 
   // Get cart.
   mock
-    .onGet(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/`)
+    .onGet(`${api}/carts/${cartUid}/`)
     .reply(200, responses.getCartSuccess);
 
-  // Get customer.
+  // Customer search.
   mock
-    .onGet(`${apiBase}carts/customers/e7fb8831-4a83-468e-8eec-593185909f18/`)
-    .reply(200, responses.getCustomerSuccess);
-
-  // Create customer.
-  mock
-    .onPost(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/customers/`)
-    .reply(200, responses.createCustomerSuccess);
-
-  // Update customer.
-  mock
-    .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/customers/e7fb8831-4a83-468e-8eec-593185909f18/`)
-    .reply(200, responses.createCustomerSuccess);
-
-  // Get shipping options.
-  mock
-    .onGet(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/`)
-    .reply(200, responses.getShippingOptionsSuccess);
-
-  // Select shipping option.
-  mock
-    .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/f5e3ad54-e097-4b6d-8db0-3986916cfb86/`)
-    .reply(200, responses.selectShippingOptionSuccess);
-
-  // Select shipping option (alt uid).
-  mock
-    .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/shipping-options/f5e68a7d-22ad-4733-b730-a9ef8c4c6bb0/`)
-    .reply(200, responses.selectShippingOptionSuccess);
-
-  // Apply promo code.
-  mock
-    .onPut(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/promo-codes/`)
-    .reply(200, responses.applyPromoCodeSuccess);
-
-  // Get payment methods.
-  mock
-    .onGet(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/payment-methods/`)
-    .reply(200, responses.getPaymentMethodsSuccess);
-
-  // Make payment.
-  mock
-    .onPost(`${apiBase}carts/8c2756b2-f018-4c27-a025-c31fca7e482b/payments/`)
-    .reply(200, responses.makePaymentSuccess);
-
-
-  // TODO: remove these soon...
-
-  mock
-    .onGet(`${apiBase}customer-search/`)
+    .onGet(`${api}/customer-search/`)
     .reply(request => {
       const response = request.params.email === 'test@test.com'
                      ? responses.customerSearchHasAccount
@@ -76,7 +35,53 @@ export function setupCheckoutAxiosMock() {
       return [200, response];
     });
 
+  // Create account.
   mock
-    .onPost(`${apiBase}customer-new/`)
+    .onPost(`${api}/customer-new/`)
     .reply(200, responses.createAccountSuccess);
+
+  // Get customer.
+  mock
+    .onGet(`${api}/carts/customers/${customerUid}/`)
+    .reply(200, responses.getCustomerSuccess);
+
+  // Create customer.
+  mock
+    .onPost(`${api}/carts/${cartUid}/customers/`)
+    .reply(200, responses.createCustomerSuccess);
+
+  // Update customer.
+  mock
+    .onPut(`${api}/carts/${cartUid}/customers/${customerUid}/`)
+    .reply(200, responses.createCustomerSuccess);
+
+  // Get shipping options.
+  mock
+    .onGet(`${api}/carts/${cartUid}/shipping-options/`)
+    .reply(200, responses.getShippingOptionsSuccess);
+
+  // Select shipping option.
+  mock
+    .onPut(`${api}/carts/${cartUid}/shipping-options/${shippingOption1Uid}/`)
+    .reply(200, responses.selectShippingOptionSuccess);
+
+  // Select shipping option (alt uid).
+  mock
+    .onPut(`${api}/carts/${cartUid}/shipping-options/${shippingOption2Uid}/`)
+    .reply(200, responses.selectShippingOptionSuccess);
+
+  // Apply promo code.
+  mock
+    .onPut(`${api}/carts/${cartUid}/promo-codes/`)
+    .reply(200, responses.applyPromoCodeSuccess);
+
+  // Get payment methods.
+  mock
+    .onGet(`${api}/carts/${cartUid}/payment-methods/`)
+    .reply(200, responses.getPaymentMethodsSuccess);
+
+  // Make payment.
+  mock
+    .onPost(`${api}/carts/${cartUid}/payments/`)
+    .reply(200, responses.makePaymentSuccess);
 }
