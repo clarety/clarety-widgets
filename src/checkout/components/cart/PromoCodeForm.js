55a095e4263c62edd6c88c97774aafc5dccda4d8
Adam Pritchard <apritchard@iworks.com.au>
2019-08-29 16:31:13 +1000

    Migrate promo code form from context to redux;

--- src/checkout/components/cart/PromoCodeForm.js
+++ src/checkout/components/cart/PromoCodeForm.js
@@ -3,66 +3,36 @@ import { connect } from 'react-redux';
 import { Col, Form } from 'react-bootstrap';
 import BlockUi from 'react-block-ui';
 import 'react-block-ui/style.css';
-import { TextInput, Button } from 'checkout/components';
+import { RxTextInput, Button } from 'checkout/components';
 import { statuses, applyPromoCode } from 'checkout/actions';
-import { FormContext } from 'checkout/utils';
 
 class _PromoCodeForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      formData: {},
-      errors: [],
-      onChange: this.onChangeField,
-    };
-  }
-
-  onChangeField = (field, value) => {
-    this.setState(prevState => ({
-      formData: {
-        ...prevState.formData,
-        [field]: value,
-      },
-    }));
-  };
-
   onPressApplyDiscount = event => {
     event.preventDefault();
 
-    const { promoCode } = this.state.formData;
+    const { promoCode } = this.props.formData;
     this.props.applyPromoCode(promoCode);
   };
 
-  componentDidUpdate(prevProps) {
-    if (this.props.errors !== prevProps.errors) {
-      this.setState({ errors: this.props.errors });
-    }
-  }
-
   render() {
-    const { promoCode } = this.state.formData;
     const { isBusy } = this.props;
 
     return (
       <BlockUi tag="div" blocking={isBusy} loader={<span></span>}>
-        <FormContext.Provider value={this.state}>
-          <Form onSubmit={this.onPressApplyDiscount} className="promo-code-form">
-            <Form.Row>
-              <Col>
-                <TextInput field="promoCode" placeholder="Discount Code" />
-              </Col>
-              <Col xs="auto">
-                <Button
-                  title="Apply"
-                  type="submit"
-                  isBusy={isBusy}
-                  disabled={!promoCode}
-                />
-              </Col>
-            </Form.Row>
-          </Form>
-        </FormContext.Provider>
+        <Form onSubmit={this.onPressApplyDiscount} className="promo-code-form">
+          <Form.Row>
+            <Col>
+              <RxTextInput field="promoCode" placeholder="Discount Code" />
+            </Col>
+            <Col xs="auto">
+              <Button
+                title="Apply"
+                type="submit"
+                isBusy={isBusy}
+              />
+            </Col>
+          </Form.Row>
+        </Form>
       </BlockUi>
     );
   }
@@ -71,6 +41,7 @@ class _PromoCodeForm extends React.Component {
 const mapStateToProps = state => {
   return {
     isBusy: state.status === statuses.busyPromoCode,
+    formData: state.formData,
     errors: state.errors,
   };
 };
