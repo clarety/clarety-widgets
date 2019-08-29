4b22341aaabd8a9063c8ad81eccc3ec79082a840
Adam Pritchard <apritchard@iworks.com.au>
2019-08-29 16:32:31 +1000

    Delete old form context based inputs;

--- src/checkout/components/index.js
+++ src/checkout/components/index.js
@@ -3,17 +3,7 @@ export * from './cart/CartSummary';
 export * from './cart/PromoCodeForm';
 
 export * from './inputs/Button';
-export * from './inputs/CardNumberInput';
-export * from './inputs/CcvInput';
-export * from './inputs/CheckboxInput';
-export * from './inputs/DobInput';
-export * from './inputs/ExpiryInput';
-export * from './inputs/PhoneInput';
-export * from './inputs/StateInput';
-export * from './inputs/TextInput';
-
 export * from './inputs/BaseInput';
-
 export * from './inputs/RxTextInput';
 export * from './inputs/RxCheckboxInput';
 export * from './inputs/RxPhoneInput';
@@ -23,9 +13,7 @@ export * from './inputs/RxCardNumberInput';
 export * from './inputs/RxCcvInput';
 export * from './inputs/RxExpiryInput';
 
-export * from './panels/BasePanel';
 export * from './panels/RxBasePanel';
-
 export * from './panels/PanelHeader';
 export * from './panels/LoginPanel';
 export * from './panels/PaymentDetailsPanel';
@@ -33,6 +21,5 @@ export * from './panels/PersonalDetailsPanel';
 export * from './panels/ShippingDetailsPanel';
 export * from './panels/ShippingOptionsPanel';
 export * from './panels/PanelStack';
-
 
 export * from './Checkout';
