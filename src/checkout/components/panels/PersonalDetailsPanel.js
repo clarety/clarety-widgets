0cdbab88533647c76d4f3991e2342f4b4b137249
Adam Pritchard <apritchard@iworks.com.au>
2019-08-29 14:57:19 +1000

    Migrate personal details panel from context to redux;

--- src/checkout/components/panels/PersonalDetailsPanel.js
+++ src/checkout/components/panels/PersonalDetailsPanel.js
@@ -3,17 +3,15 @@ import { connect } from 'react-redux';
 import { Form, Col } from 'react-bootstrap';
 import BlockUi from 'react-block-ui';
 import 'react-block-ui/style.css';
-import { BasePanel, TextInput, PhoneInput, DobInput, Button } from 'checkout/components';
+import { RxBasePanel, RxTextInput, RxPhoneInput, RxDobInput, Button } from 'checkout/components';
 import { WaitPanelHeader, EditPanelHeader, DonePanelHeader } from 'checkout/components';
-import { statuses, updateFormData, nextPanel, editPanel } from 'checkout/actions';
-import { FormContext } from 'checkout/utils';
+import { statuses, nextPanel, editPanel, panelStatuses, updateFormData, setErrors } from 'checkout/actions';
 
-class _PersonalDetailsPanel extends BasePanel {
+class _PersonalDetailsPanel extends RxBasePanel {
   onPressContinue = event => {
     event.preventDefault();
 
     if (this.validate()) {
-      this.props.updateFormData(this.state.formData);
       this.props.nextPanel();
     }
   };
@@ -30,7 +28,7 @@ class _PersonalDetailsPanel extends BasePanel {
 
     this.validateRequired('sale.source', errors);
 
-    this.setState({ errors });
+    this.props.setErrors(errors);
     return errors.length === 0;
   }
 
@@ -40,7 +38,6 @@ class _PersonalDetailsPanel extends BasePanel {
     }
 
     if (this.props.errors !== prevProps.errors) {
-      this.setState({ errors: this.props.errors });
       this.checkForErrors();
     }
   }
@@ -57,12 +54,13 @@ class _PersonalDetailsPanel extends BasePanel {
     if (this.hasError('customer.dateOfBirthMonth')) foundError = true;
     if (this.hasError('customer.dateOfBirthYear'))  foundError = true;
 
-    if (foundError) {
+    if (foundError && this.props.status !== panelStatuses.edit) {
       const { index, editPanel } = this.props;
       editPanel(index);
     }
   }
 
+  // TODO: this shouldn't be done in the component...
   prefillCustomerData(customer) {
     let formData = {};
 
@@ -79,7 +77,7 @@ class _PersonalDetailsPanel extends BasePanel {
       };
     }
 
-    this.setState({ formData });
+    this.props.updateFormData(formData);
   }
 
   renderWait() {
@@ -96,63 +94,61 @@ class _PersonalDetailsPanel extends BasePanel {
         <EditPanelHeader number="2" title="Personal Details" />
         
         <BlockUi tag="div" blocking={isBusy} loader={<span></span>}>
-          <FormContext.Provider value={this.state}>
-            <Form onSubmit={this.onPressContinue}>
-              <Form.Row>
-                <Col sm={6}>
-                  <TextInput field="customer.firstName" placeholder="First Name *" />
-                </Col>
-                <Col sm={6}>
-                  <TextInput field="customer.lastName" placeholder="Last Name *" />
-                </Col>
-              </Form.Row>
+          <Form onSubmit={this.onPressContinue}>
+            <Form.Row>
+              <Col sm={6}>
+                <RxTextInput field="customer.firstName" placeholder="First Name *" />
+              </Col>
+              <Col sm={6}>
+                <RxTextInput field="customer.lastName" placeholder="Last Name *" />
+              </Col>
+            </Form.Row>
 
-              <Form.Row>
-                <Col sm={6}>
-                  <PhoneInput field="customer.phone1" placeholder="Home Phone" />
-                </Col>
-                <Col sm={6}>
-                  <PhoneInput field="customer.phone2" placeholder="Work Phone" />
-                </Col>
-              </Form.Row>
+            <Form.Row>
+              <Col sm={6}>
+                <RxPhoneInput field="customer.phone1" placeholder="Home Phone" />
+              </Col>
+              <Col sm={6}>
+                <RxPhoneInput field="customer.phone2" placeholder="Work Phone" />
+              </Col>
+            </Form.Row>
 
-              <Form.Row>
-                <Col sm={6}>
-                  <PhoneInput field="customer.mobile" placeholder="Mobile Phone" />
-                </Col>
-                <Col sm={6}>
-                </Col>
-              </Form.Row>
+            <Form.Row>
+              <Col sm={6}>
+                <RxPhoneInput field="customer.mobile" placeholder="Mobile Phone" />
+              </Col>
+              <Col sm={6}>
+              </Col>
+            </Form.Row>
 
-              <Form.Row>
-                <Col>Date of Birth *</Col>
-              </Form.Row>
+            <Form.Row>
+              <Col>Date of Birth *</Col>
+            </Form.Row>
 
-              <DobInput
-                field="customer.dateOfBirth"
-                dayField="customer.dateOfBirthDay"
-                monthField="customer.dateOfBirthMonth"
-                yearField="customer.dateOfBirthYear"
-              />
+            <RxDobInput
+              field="customer.dateOfBirth"
+              dayField="customer.dateOfBirthDay"
+              monthField="customer.dateOfBirthMonth"
+              yearField="customer.dateOfBirthYear"
+            />
 
-              <Form.Row>
-                <Col>
-                  <TextInput field="sale.source" placeholder="How did you hear about us? *" />
-                </Col>
-              </Form.Row>
+            <Form.Row>
+              <Col>
+                <RxTextInput field="sale.source" placeholder="How did you hear about us? *" />
+              </Col>
+            </Form.Row>
 
-              <div className="text-right mt-3">
-                <Button title="Continue" type="submit" />
-              </div>
-            </Form>
-          </FormContext.Provider>
+            <div className="text-right mt-3">
+              <Button title="Continue" type="submit" />
+            </div>
+          </Form>
         </BlockUi>
       </div>
     );
   }
 
   renderDone() {
-    const { formData } = this.state;
+    const { formData } = this.props;
     const firstName = formData['customer.firstName'];
     const lastName = formData['customer.lastName'];
     const phone = formData['customer.phone1'] || formData['customer.phone2'] || formData['customer.mobile'];
@@ -174,14 +170,16 @@ const mapStateToProps = state => {
   return {
     isBusy: state.status === statuses.busy,
     customer: state.cart.customer,
+    formData: state.formData,
     errors: state.errors,
   };
 };
 
 const actions = {
-  updateFormData: updateFormData,
   nextPanel: nextPanel,
   editPanel: editPanel,
+  updateFormData: updateFormData,
+  setErrors: setErrors,
 };
 
 export const PersonalDetailsPanel = connect(mapStateToProps, actions)(_PersonalDetailsPanel);
