d4e25e3edfee80830770c6472f93b0fc61a822b1
Adam Pritchard <apritchard@iworks.com.au>
2019-09-01 13:18:27 +1000

    Remove 'api' folder from actions;

--- src/checkout/actions/index.js
+++ src/checkout/actions/index.js
@@ -1,10 +1,9 @@
 export * from './types';
 
+export * from './auth-actions';
+export * from './cart-actions';
 export * from './error-actions';
 export * from './panel-actions';
+export * from './payment-actions';
 export * from './form-data-actions';
 export * from './validation-actions';
-
-export * from './api/auth-actions';
-export * from './api/cart-actions';
-export * from './api/payment-actions';
