71e1446ba99085cd6a7b4d3bfe39ba4b66f11b6b
Adam Pritchard <apritchard@iworks.com.au>
2019-09-24 16:52:11 +1000

    Update email validation regex;

--- src/registrations/components/panels/DetailsPanel.js
+++ src/registrations/components/panels/DetailsPanel.js
@@ -281,7 +281,9 @@ export class _DetailsPanel extends React.Component {
 
   validateEmail(field, formData, errors) {
     const { intl } = this.props;
-    const isValid = /^.+@.+\..+$/.test(formData[field]);
+    // NOTE: giant ugly regex from: https://emailregex.com/
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isValid = regex.test(formData[field]);
     if (!isValid) {
       const message = intl.formatMessage({ id: 'validation.email' });
       errors.push({ field: field, message: message });
--- src/shared/utils/validation-utils.js
+++ src/shared/utils/validation-utils.js
@@ -1,3 +1,5 @@
 export function validateEmail(email) {
-  return /^.+@.+\..+$/.test(email);
+  // NOTE: giant ugly regex from: https://emailregex.com/
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
 }
