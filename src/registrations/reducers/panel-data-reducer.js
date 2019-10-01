c4477da5707c40323fc60ae3e3399475114209b8
Adam Pritchard <apritchard@iworks.com.au>
2019-10-01 15:35:04 +1000

    Tidy up 'panel data' reducer;

--- src/registrations/reducers/panel-data-reducer.js
+++ src/registrations/reducers/panel-data-reducer.js
@@ -8,123 +8,136 @@ const initialState = {
 
 export const panelDataReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.panelDataSetEvent:
-      return {
-        ...state,
-        eventId: action.eventId,
-      };
-
-    case types.panelDataResetEvent:
-      return {
-        ...state,
-        eventId: initialState.eventId,
-      };
-
-    case types.panelDataSetQtys:
-      const participants = [];
-      for (const [key, value] of Object.entries(action.qtys)) {
-        for (let index = 0; index < value; index++) {
-          participants.push({ type: key });
-        }
-      }
-
-      return {
-        ...state,
-        qtys: action.qtys,
-        participants,
-      };
-
-    case types.panelDataResetQtys:
-      return {
-        ...state,
-        qtys: initialState.qtys,
-      };
-
-    case types.panelDataSetFirstNames:
-      return {
-        ...state,
-        participants: state.participants.map((participant, index) =>
-          ({
-            ...participant,
-            customer: {
-              ...participant.customer,
-              firstName: action.firstNames[index],
-            },
-          })
-        ),
-      };
-
-    case types.panelDataResetFirstNames:
-      return {
-        ...state,
-        participants: [],
-      };
-
-    case types.panelDataSetDetails:
-      return {
-        ...state,
-        participants: state.participants.map((participant, index) => {
-          if (index === action.index) {
-            return {
-              ...participant,
-              customer: action.customerForm,
-              extendForm: action.extendForm,
-              errors: [],
-            };
-          } else {
-            return participant;
-          }
-        }),
-      };
-
-    case types.panelDataSetAdditionalData:
-      return {
-        ...state,
-        participants: state.participants.map((participant, index) => {
-          if (index === action.index) {
-            return {
-              ...participant,
-              additionalData: action.additionalData,
-            };
-          } else {
-            return participant;
-          }
-        }),
-      };
-
-    case types.panelDataSetErrors:
-      return {
-        ...state,
-        participants: state.participants.map((participant, index) => {
-          if (index === action.index) {
-            return {
-              ...participant,
-              errors: action.errors,
-            };
-          } else {
-            return participant;
-          }
-        }),
-      };
-
-    case types.panelDataResetDetails:
-      return {
-        ...state,
-        participants: state.participants.map((participant, index) => {
-          if (index === action.index) {
-            return { 
-              ...participant,
-              customer: undefined,
-              extendForm: undefined,
-              errors: undefined,
-            };
-          } else {
-            return participant;
-          }
-        }),
-      };
-
-    default:
-      return state;
+    case types.panelDataSetEvent:          return setEvent(state, action);
+    case types.panelDataResetEvent:        return resetEvent(state, action);
+    case types.panelDataSetQtys:           return setQtys(state, action);
+    case types.panelDataResetQtys:         return resetQtys(state, action);
+    case types.panelDataSetFirstNames:     return setFirstNames(state, action);
+    case types.panelDataResetFirstNames:   return resetFirstNames(state, action);
+    case types.panelDataSetDetails:        return setDetails(state, action);
+    case types.panelDataSetAdditionalData: return setAdditionalData(state, action);
+    case types.panelDataSetErrors:         return setErrors(state, action);
+    case types.panelDataResetDetails:      return resetDetails(state, action);
+    default:                               return state;
   }
 };
+
+function setEvent(state, action) {
+  return {
+    ...state,
+    eventId: action.eventId,
+  };
+}
+
+function resetEvent(state, action) {
+  return {
+    ...state,
+    eventId: null,
+  };
+}
+
+function setQtys(state, action) {
+  const participants = [];
+  for (const [key, value] of Object.entries(action.qtys)) {
+    for (let index = 0; index < value; index++) {
+      participants.push({ type: key });
+    }
+  }
+
+  return {
+    ...state,
+    qtys: action.qtys,
+    participants,
+  };
+}
+
+function resetQtys(state, action) {
+  return {
+    ...state,
+    qtys: initialState.qtys,
+  };
+}
+
+function setFirstNames(state, action) {
+  return {
+    ...state,
+    participants: state.participants.map((participant, index) =>
+      ({
+        ...participant,
+        customer: {
+          ...participant.customer,
+          firstName: action.firstNames[index],
+        },
+      })
+    ),
+  };
+}
+
+function resetFirstNames(state, action) {
+  return {
+    ...state,
+    participants: [],
+  };
+}
+
+function setDetails(state, action) {
+  return {
+    ...state,
+    participants: state.participants.map((participant, index) => {
+      if (index !== action.index) return participant;
+
+      return {
+        ...participant,
+        customer: action.customerForm,
+        extendForm: action.extendForm,
+        errors: [],
+      };
+    }),
+  };
+}
+
+function resetDetails(state, action) {
+  return {
+    ...state,
+    participants: state.participants.map((participant, index) => {
+      if (index !== action.index) return participant;
+
+      return {
+        ...participant,
+        customer: {
+          firstName: participant.customer.firstName,
+        },
+        extendForm: {},
+        errors: [],
+      };
+    }),
+  };
+}
+
+function setAdditionalData(state, action) {
+  return {
+    ...state,
+    participants: state.participants.map((participant, index) => {
+      if (index !== action.index) return participant;
+
+      return {
+        ...participant,
+        additionalData: action.additionalData,
+      };
+    }),
+  };
+}
+
+function setErrors(state, action) {
+  return {
+    ...state,
+    participants: state.participants.map((participant, index) => {
+      if (index !== action.index) return participant;
+
+      return {
+        ...participant,
+        errors: action.errors,
+      };
+    }),
+  };
+}
