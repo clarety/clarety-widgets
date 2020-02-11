f3835e0e300e25f4b664f0129ad38ccf7eeacc99
Adam Pritchard <apritchard@iworks.com.au>
2020-02-11 14:43:59 +1000

    Remove 'widget' subfolder of 'panels';

--- src/donate/components/index.js
+++ src/donate/components/index.js
@@ -6,16 +6,16 @@ export * from './amounts/VariableAmountLg';
 
 export * from './misc/StepIndicator';
 
-export * from './panels/widget/CustomerPanel';
-export * from './panels/widget/CustomerConnect';
-export * from './panels/widget/DonationPanel';
-export * from './panels/widget/DonationConnect';
-export * from './panels/widget/FundraisingPanel';
-export * from './panels/widget/FundraisingConnect';
-export * from './panels/widget/PaymentPanel';
-export * from './panels/widget/PaymentConnect';
-export * from './panels/widget/SuccessPanel';
-export * from './panels/widget/SuccessConnect';
+export * from './panels/CustomerPanel';
+export * from './panels/CustomerConnect';
+export * from './panels/DonationPanel';
+export * from './panels/DonationConnect';
+export * from './panels/FundraisingPanel';
+export * from './panels/FundraisingConnect';
+export * from './panels/PaymentPanel';
+export * from './panels/PaymentConnect';
+export * from './panels/SuccessPanel';
+export * from './panels/SuccessConnect';
 
 export * from './DonateWidget';
 export * from './DonatePage';
