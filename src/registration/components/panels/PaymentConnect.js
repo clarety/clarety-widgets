734e02baa64c2fb16673c79b549e155a9dee45f5
Adam Pritchard <apritchard@iworks.com.au>
2020-02-14 14:03:59 +1000

    Update registration demo to use donate payment panel;

--- src/registration/components/panels/PaymentConnect.js
+++ src/registration/components/panels/PaymentConnect.js
@@ -1,21 +1,26 @@
+import { getSetting } from 'shared/selectors';
+import { getFormData, getErrors } from 'form/selectors';
+import { setErrors } from 'form/actions';
 import { submitRegistration } from 'registration/actions';
 import { getCart, getPaymentMethod } from 'registration/selectors';
 
-export class RegistrationPaymentConnect {
+export class PaymentConnect {
   static mapStateToProps = (state, ownProps) => {
-    const paymentMethod = getPaymentMethod(state);
-
     return {
       isBusy: state.status === 'busy',
-      errors: state.errors,
-      paymentMethod: paymentMethod,
+      errors: getErrors(state),
+      paymentMethod: getPaymentMethod(state),
+      formData: getFormData(state),
       submitBtnTitle: 'Submit Registration',
       cart: getCart(state),
+      errors: getErrors(state),
+      variant: getSetting(state, 'variant'),
     };
   };
   
   static actions = {
-    onShowPanel: () => ({ type: 'NO_OP' }),
+    onShowPanel: () => async () => true,
     onSubmit: submitRegistration,
+    setErrors: setErrors,
   };
 }
