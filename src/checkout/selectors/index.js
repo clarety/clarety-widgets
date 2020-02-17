c6df122458a238a1b6fd243032082a325bdd6fc7
Adam Pritchard <apritchard@iworks.com.au>
2020-02-17 15:25:49 +1000

    Update checkout payment action to get payment method from state;

--- src/checkout/selectors/index.js
+++ src/checkout/selectors/index.js
@@ -1,8 +1,8 @@
-export const hasSelectedShippingOption = state => {
+export const hasSelectedShippingOption = (state) => {
   return !!state.formData['sale.shippingOption'];
 };
 
-export const getSelectedShippingOptionLabel = state => {
+export const getSelectedShippingOptionLabel = (state) => {
   const { shippingOptions, sale } = state.cart;
 
   if (shippingOptions && sale) {
@@ -14,8 +14,9 @@ export const getSelectedShippingOptionLabel = state => {
   return '';
 };
 
-export const getPaymentMethods = state => state.cart.paymentMethods;
+export const getPaymentMethods = (state) => state.cart.paymentMethods;
 
-const getLoginPanel = state => {
-  return state.panels.loginPanel;
+export const getPaymentMethod = (state, type) => {
+  const methods = getPaymentMethods(state);
+  return methods.find(method => method.type === type);
 };
