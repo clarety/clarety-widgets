86ed396a11ba90125e52f11973a7cc1397c0d7f4
Adam Pritchard <apritchard@iworks.com.au>
2020-02-28 16:01:37 +1000

    Add a 'cleanFn' prop to TextInput; Update PostcodeInput, AccountNumberInput, BsbInput, and CardNameInput to wrap TextInput and provide a clean function;

--- src/form/components/inputs/payment/AccountNumberInput.js
+++ src/form/components/inputs/payment/AccountNumberInput.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Form } from 'react-bootstrap';
-import { updateFormData } from 'form/actions';
-import { getValidationError } from 'form/utils';
-import { FieldError } from 'form/components';
+import { TextInput } from 'form/components';
 
-const _AccountNumberInput = ({ value, placeholder, error, onChange, required }) => {
-  if (placeholder && !required) placeholder += ' (Optional)';
-
-  return (
-    <React.Fragment>
-      <Form.Control
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        isInvalid={!!error}
-      />
-      <FieldError error={error} />
-    </React.Fragment>
-  );
-};
-
-const mapStateToProps = (state, { field }) => {
-  return {
-    value: state.formData[field] || '',
-    error: getValidationError(field, state.errors),
-  };
-};
-
-const cleanAccountNumber = value => value.replace(/[^0-9]/g, '').substring(0, 10);
-
-const mapDispatchToProps = (dispatch, { field }) => {
-  return {
-    onChange: event => dispatch(
-      updateFormData(field, cleanAccountNumber(event.target.value))
-    ),
-  };
-};
-
-export const AccountNumberInput = connect(mapStateToProps, mapDispatchToProps)(_AccountNumberInput);
+export const AccountNumberInput = (props) => (
+  <TextInput
+    cleanFn={value => value.replace(/[^0-9]/g, '').substring(0, 10)}
+    {...props}
+  />
+);
--- src/form/components/inputs/payment/BsbInput.js
+++ src/form/components/inputs/payment/BsbInput.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Form } from 'react-bootstrap';
-import { updateFormData } from 'form/actions';
-import { getValidationError } from 'form/utils';
-import { FieldError } from 'form/components';
+import { TextInput } from 'form/components';
 
-const _BsbInput = ({ value, placeholder, error, onChange, required }) => {
-  if (placeholder && !required) placeholder += ' (Optional)';
-
-  return (
-    <React.Fragment>
-      <Form.Control
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        isInvalid={!!error}
-      />
-      <FieldError error={error} />
-    </React.Fragment>
-  );
-};
-
-const mapStateToProps = (state, { field }) => {
-  return {
-    value: state.formData[field] || '',
-    error: getValidationError(field, state.errors),
-  };
-};
-
-const cleanBsb = value => value.replace(/[^0-9]/g, '').substring(0, 6);
-
-const mapDispatchToProps = (dispatch, { field }) => {
-  return {
-    onChange: event => dispatch(
-      updateFormData(field, cleanBsb(event.target.value))
-    ),
-  };
-};
-
-export const BsbInput = connect(mapStateToProps, mapDispatchToProps)(_BsbInput);
+export const BsbInput = (props) => (
+  <TextInput
+    cleanFn={value => value.replace(/[^0-9]/g, '').substring(0, 6)}
+    {...props}
+  />
+);
