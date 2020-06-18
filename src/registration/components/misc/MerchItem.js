6fdaca26d386755d368c9d0ed555f0c0e386b5fe
Adam Pritchard <apritchard@iworks.com.au>
2020-06-18 12:20:29 +1000

    Show 'sold out' in MerchItem;

--- src/registration/components/misc/MerchItem.js
+++ src/registration/components/misc/MerchItem.js
@@ -4,38 +4,53 @@ import { Button } from 'form/components';
 import { QtyInput } from 'registration/components';
 import { Currency } from 'shared/components';
 
-export const MerchItem = ({ merchItem, qtys, onShowQtys, onChangeQty }) => {
-  const hasSizes = !!(merchItem.products && merchItem.products.length);
+export class MerchItem extends React.Component {
+  render() {
+    const { merchItem } = this.props;
 
-  return (
-    <div className="merch-item">
-      <Image src={merchItem.image} fluid className="merch-item__image" />
-      <h3 className="merch-item__name">{merchItem.name}</h3>
-      <h4 className="merch-item__price"><Currency amount={merchItem.sell} /></h4>
-      <div className="merch-item__description">{merchItem.shortDescription}</div>
+    return (
+      <div className="merch-item">
+        <Image src={`https://neutrinodata.s3.amazonaws.com/a21/cmsimages/thumb/${merchItem.image}`} fluid className="merch-item__image" />
+        <h3 className="merch-item__name">{merchItem.name}</h3>
+        <h4 className="merch-item__price"><Currency amount={merchItem.sell} /></h4>
+        <div className="merch-item__description">{merchItem.shortDescription}</div>
+        <div className="merch-item__action">
+          {this.renderMerchAction()}
+        </div>
+      </div>
+    );
+  }
 
-      {hasSizes
-        ? <MerchItemSizes merchItem={merchItem} qtys={qtys} onShowQtys={onShowQtys} />
-        : <QtyInput value={qtys} onChange={value => onChangeQty(merchItem.offerId, undefined, value)} />
-      }
-    </div>
-  );
-};
+  renderMerchAction() {
+    const { merchItem, qtys, onShowQtys, onChangeQty } = this.props;
+    const hasSizes = !!(merchItem.products && merchItem.products.length);
 
-const MerchItemSizes = ({ merchItem, qtys, onShowQtys }) => (
-  <div className="merch-item-sizes">
-    {merchItem.products.map(product => {
-      const qty = qtys && qtys[product.productId];
-      if (!qty) return null;
+    if (merchItem.soldOut) {
+      return <Button disabled>Sold Out!</Button>;
+    }
 
+    if (hasSizes) {
       return (
-        <div className="merch-item-size" key={product.productId}>
-          <div className="merch-item-size__name">{product.name}</div>
-          <div className="merch-item-size__qty">{qty}</div>
+        <div className="merch-item-sizes">
+          {merchItem.products.map(product => {
+            const qty = qtys && qtys[product.productId];
+            if (!qty) return null;
+
+            return (
+              <div className="merch-item-size" key={product.productId}>
+                <div className="merch-item-size__name">{product.name}</div>
+                <div className="merch-item-size__qty">{qty}</div>
+              </div>
+            );
+          })}
+
+          <Button onClick={() => onShowQtys(merchItem)}>Select Sizes</Button>
         </div>
       );
-    })}
+    }
 
-    <Button onClick={() => onShowQtys(merchItem)}>Select Sizes</Button>
-  </div>
-);
+    return (
+      <QtyInput value={qtys} onChange={value => onChangeQty(merchItem.offerId, undefined, value)} />
+    );
+  }
+}
