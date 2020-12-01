d750db8d1a59db6c06c26a3ca5c8198258a33cb0
Adam Pritchard <apritchard@iworks.com.au>
2020-12-01 12:58:54 +1000

    add sos progress translations;

--- src/lead-gen/components/misc/SosProgress.js
+++ src/lead-gen/components/misc/SosProgress.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
+import { t } from 'shared/translations';
 
 export const SosProgress = ({ sos }) => (
   <div className="sos-progress">
-    <p>{sos.current} signatures <span className="goal-text">of {sos.goal}</span></p>
+    <p>
+      {t('sos-signatures', '{{count}} have already signed...', { count: sos.current })}
+      {' '}
+      <span className="goal-text">{t('sos-goal', "Let's get to {{goal}}!", { goal: sos.goal })}</span>
+    </p>
+
     <ProgressBar now={sos.current/sos.goal * 100} />
   </div>
 );
