ef5dda3629ab2bdc8017a5045d56eb3d38b5710a
Adam Pritchard <apritchard@iworks.com.au>
2024-01-10 10:12:26 +1000

    61322: checkout stripe payment form; tabs layout;

--- src/checkout/components/misc/StripePaymentForm.js
+++ src/checkout/components/misc/StripePaymentForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PaymentElement } from '@stripe/react-stripe-js';
 
-export function StripePaymentForm({ paymentMethod, customerInfo, layout = 'accordion' }) {
+export function StripePaymentForm({ paymentMethod, customerInfo, layout = 'tabs' }) {
   return (
     <PaymentElement
       className="stripe-payment-form"
