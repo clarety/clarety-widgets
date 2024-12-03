b7f7663c95515d5fb37166b608afbc52e80425e7
Adam Pritchard <apritchard@iworks.com.au>
2024-12-03 10:05:18 +1000

    63554: subscribe widget confirmation text color

--- src/subscribe/components/panels/ConfirmPanel.js
+++ src/subscribe/components/panels/ConfirmPanel.js
@@ -10,11 +10,12 @@ export class ConfirmPanel extends BasePanel {
   renderEdit() {
     const { layout, settings } = this.props;
     const content = settings.confirmContent || t('opt-in-confirm', 'Thank you for subscribing!');
+    const textColor = settings.textColor || 'dark';
 
     return (
       <PanelContainer layout={layout} status="edit" className="confirm-panel">
         <PanelBody layout={layout} status="edit">
-          <p className="confirm-panel-content">
+          <p className={`confirm-panel-content text-${textColor}`}>
             {content}
           </p>
         </PanelBody>
