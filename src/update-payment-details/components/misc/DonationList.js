9f576107524a4bc00e46a784b6a11f79e9c2b4ca
Adam Pritchard <apritchard@iworks.com.au>
2026-07-29 11:46:32 +1000

    65288: update-payment-details-widget; feedback;

--- src/update-payment-details/components/misc/DonationList.js
+++ src/update-payment-details/components/misc/DonationList.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { t } from 'shared/translations';
 import { recurrenceStatusClassName } from 'update-payment-details/utils';
 
-export function DonationList({ recurringDonations }) {
+export function DonationList({ recurringDonations, displayStatusFn }) {
   return (
     <div className="donation-list">
       {recurringDonations.map((recurringDonation) =>
         <div key={recurringDonation.salelinePaymentUid} className="donation-list-item">
           <div className={recurrenceStatusClassName(recurringDonation.status)}>
-            {recurringDonation.status}
+            {displayStatusFn
+              ? displayStatusFn(recurringDonation)
+              : t(recurringDonation.status, recurringDonation.status)
+            }
           </div>
           <div className="details">
-            {recurringDonation.amount} {recurringDonation.paymentSchedule}
+            {recurringDonation.amount} {t(recurringDonation.paymentSchedule, recurringDonation.paymentSchedule)}
           </div>
         </div>
       )}
