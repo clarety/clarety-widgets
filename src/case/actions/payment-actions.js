cf8edcf13877fae5364bdf57ef40c3128538461e
Adam Pritchard <apritchard@iworks.com.au>
2026-08-11 11:46:23 +1000

    66966: case payment form confirm after authorise fix;

--- src/case/actions/payment-actions.js
+++ src/case/actions/payment-actions.js
@@ -1,60 +1,27 @@
 import { ClaretyApi } from 'shared/utils/clarety-api';
-import { setStatus, statuses } from 'shared/actions';
 import { getSetting } from 'shared/selectors';
-import { preparePayment, attemptPayment, handlePaymentError, handlePaymentAuthorise, makePaymentSuccess } from 'checkout/actions';
-import { getPaymentMethod } from 'checkout/selectors';
+import { makePayment as makeCheckoutPayment } from 'checkout/actions';
 import { getSubmitCasePostData } from 'case/selectors';
 import { showCaseConfirmation } from 'case/actions';
 
 export const makePayment = (paymentData) => {
-  return async (dispatch, getState) => {
-    dispatch(setStatus(statuses.busy));
-
-    const state = getState();
-
-    const paymentMethod = getPaymentMethod(state, paymentData.type);
-
-    // Prepare payment.
-    const prepared = await dispatch(preparePayment(paymentData, paymentMethod));
-    if (!prepared) return false;
-
-    // Attempt payment.
-    const result = await dispatch(attemptPayment(paymentData, paymentMethod));
-    if (!result) return false;
-
-    // Handle result.
-    return await dispatch(handlePaymentResult(result, paymentData, paymentMethod));
-  };
+  /**
+   * use the checkout payment actions,
+   * but provide an on complete handler to update the case sale+stage and show the case widget confirmation.
+   */
+  return makeCheckoutPayment(paymentData, onPaymentComplete);
 };
 
-const handlePaymentResult = (result, paymentData, paymentMethod) => {
-  return async (dispatch, getState) => {
-    // TODO: temp api fix.
-    if (result.status === 'Complete') result.status = 'complete';
+const onPaymentComplete = async (result, paymentData, paymentMethod, dispatch, getState) => {
+  const state = getState();
+  const caseUid = getSetting(state, 'caseUid');
+  const paidStage = getSetting(state, 'paidStage');
 
-    switch (result.status) {
-      case 'error':     return dispatch(handlePaymentError(result, paymentData, paymentMethod));
-      case 'authorise': return dispatch(handlePaymentAuthorise(result, paymentData, paymentMethod));
-      case 'complete':  return dispatch(handlePaymentComplete(result, paymentData, paymentMethod));
-      default: throw new Error('handlePaymentResult not implemented for status: ' + result.status);
-    }    
-  }
-};
+  const postData = getSubmitCasePostData(state);
+  postData.cartUid = result.cartUid;
+  postData.stage = paidStage;
 
-const handlePaymentComplete = (result, paymentData, paymentMethod) => {
-  return async (dispatch, getState) => {
-    dispatch(makePaymentSuccess(result));
-    
-    const state = getState();
-    const caseUid = getSetting(state, 'caseUid');
-    const paidStage = getSetting(state, 'paidStage');
+  await ClaretyApi.post('cases/', postData);
 
-    const postData = getSubmitCasePostData(state);
-    postData.cartUid = result.cartUid;
-    postData.stage = paidStage;
-
-    await ClaretyApi.post('cases/', postData);
-
-    return dispatch(showCaseConfirmation(caseUid));
-  }
+  return dispatch(showCaseConfirmation(caseUid));
 };
